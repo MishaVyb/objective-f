38a1f33845a06644163726b7560ca9e8ab72e6bf
Misha Vybornyi <vbrn.mv@gmail.com>
2024-07-28 19:51:37 +0400

    🖋 EXCAL - Crop Image: FIX original image overlay for BIG images which was scaled down on creation (2)

--- objective-app/objective/elements/_renderScene.ts
+++ objective-app/objective/elements/_renderScene.ts
@@ -40,13 +40,12 @@ export const renderObjectiveScene = (
     ) {
       // HACK
       // el.scale -- equals 1 on creates and DOES NOT modifies during transfor (it's used only fo flip)
-      // el.rescaleX -- equals 1 on creation and modifies during transfor
+      // el.rescaleX -- equals 1 on creation and modifies during transform
       // onCreationRescaleX --
       //    if image was too big. it was scaled down on creation, and we need that factor
       //    to modify x/y properly
-      const onCreationRescaleY = el.underlyingImageHeight / el.heightAtCreation
-      const onCreationRescaleX = el.underlyingImageWidth / el.widthAtCreation
-
+      const onCreationRescaleX = el.underlyingImageWidth / el.widthAtCreation / el.rescaleX
+      const onCreationRescaleY = el.underlyingImageHeight / el.heightAtCreation / el.rescaleY
       const fullImageOverlay = newElementWith(el, {
         opacity: el.opacity / 2,
         xToPullFromImage: 0,
